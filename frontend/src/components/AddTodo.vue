754a7ae0016dbea6c0ac43ba6d31271ec2e15ef1
Erwan <erwan.fouret@gmail.com>
2018-02-14 21:11:56 +0100

    Display errors

--- frontend/src/components/AddTodo.vue
+++ frontend/src/components/AddTodo.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
+    <ul v-if="errors.length > 0" class="errors">
+      <li v-for="(error, index) in errors" :key="index">{{error}}</li>
+    </ul>
     <form>
       <input type="text" v-model="name" placeholder="name">
-      <input type="date" v-model="dueDate" placeholder="due">
+      <input type="text" v-model="dueDate" placeholder="due date">
       <input type="text" v-model="repeat" placeholder="repeat">
       <button v-on:click="add">Add</button>
     </form>
@@ -18,15 +21,17 @@ export default {
     return {
       name: '',
       dueDate: '',
-      repeat: ''
+      repeat: '',
+      errors: []
     }
   },
   methods: {
     add: function (event) {
       axios({method: 'POST', url: '/api/todos', data: {name: this.name, dueDate: this.dueDate, repeat: this.repeat}}).then(result => {
+        this.errors = []
         this.$emit('add')
       }, error => {
-        console.error(error)
+        this.errors = error.response.data.errors
       })
     }
   }
@@ -34,4 +39,8 @@ export default {
 </script>
 
 <style scoped>
+.errors {
+  background-color: #f55;
+  padding: 0.5rem 0;
+}
 </style>
