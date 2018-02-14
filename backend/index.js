e367722abff361da8a8bf140e09395a8da9c6de7
Erwan <erwan.fouret@gmail.com>
2018-02-14 21:11:39 +0100

    POST /todos validation

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const restify = require('restify')
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
+const moment = require('moment')
 
 const adapter = new FileSync('db.json')
 const db = low(adapter)
@@ -28,11 +29,31 @@ function getTodo(req, res, next) {
 }
 
 function postTodo(req, res, next) {
+  const todo = req.body
+  const errors = []
+
+  if (!todo.name) {
+    errors.push('missing-name')
+  }
+
+  if (todo.repeat && !todo.repeat.match(/^[1-9][0-9]* (day|week|month|year)s?$/)) {
+    errors.push('invalid-repeat')
+  }
+
+  if (todo.dueDate && !moment(todo.dueDate, moment.ISO_8601).isValid()) {
+    errors.push('invalid-dueDate')
+  }
+
+  if (errors.length > 0) {
+    res.send(400, {errors})
+    next()
+    return
+  }
+
   db.update('idSequence', n => n + 1).write()
   const id = db.get('idSequence').value()
-  const todo = req.body
   todo.id = id
-  todo.status = 'CREATED'
+  todo.status = 'TODO'
   db.get('todos').push(todo).write()
   res.header('Location', `/todos/${id}`)
   res.send(201)
@@ -43,7 +64,7 @@ function putTodo(req, res, next) {
   const todo = db.get('todos').find({id: Number(req.params.id)}).value()
   const newTodo = req.body
   if (newTodo.status) todo.status = req.body.status
-  if (newTodo.name) todo.name = req.body.name  
+  if (newTodo.name) todo.name = req.body.name
   // TODO update other fields
   // TODO save previous state
   db.write()
